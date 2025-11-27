77e97237429095af88f6b9183aea4aa9ecaa95f8
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2025-11-27 08:07:56 +0530

    changed sg name

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "food_orderinf_client_deploy_server_sg" {
 resource "aws_instance" "food_orderinf_client_deploy_server" {
   ami = var.ami
   subnet_id = data.aws_subnet.default.id
-  vpc_security_group_ids = [aws_security_group.build_server_sg.id]
+  vpc_security_group_ids = [aws_security_group.food_orderinf_client_deploy_server_sg.id]
   instance_type = var.instance_type
   key_name = var.key_name
   tags = {
