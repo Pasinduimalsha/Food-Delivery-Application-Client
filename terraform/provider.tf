9336adf192e30f26c338d15faa8a345c8145338e
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2026-04-05 19:23:04 +0530

    Added s3 bucket

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "food-delivery-terraform-state-pasindu"
+    key    = "food-delivery-client/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws"{
     region = var.region
 }
